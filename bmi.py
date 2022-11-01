1f4f5825ce8bf185b8445c5e283e6421e713bbb5
Ryanhuii <yhryanchan@gmail.com>
2022-11-01 10:04:12 +0800

    changes to bmi file

--- bmi.py
+++ bmi.py
@@ -3,6 +3,13 @@ def calculate_bmi(height, weight):
     print("Weight = " + str(weight))
 
     bmi = weight/(height**2)
+    if bmi < 18.5:
+        category = -1
+    elif bmi <= 25.0:
+        category = 0
+    else:
+        category = 1
     print("Your bmi is: " + str(bmi))
+    return category
 
 calculate_bmi(weight=57,height=1.73)
